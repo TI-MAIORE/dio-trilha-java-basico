ea7df67c7e448905268885e03c14c68ded5d0ad3
alencarth <timaiore24@gmail.com>
2024-11-02 18:21:10 -0300

    Aplicando o uso de constante

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -4,5 +4,13 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
