6e8e25867c33a1dfcb5f6ae62884335667a08bdf
alencarth <timaiore24@gmail.com>
2024-11-02 17:54:29 -0300

    Utilizando exemplos de declaração de variáveis seus tipos e casting

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
